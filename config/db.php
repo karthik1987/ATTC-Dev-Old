3f9c10b55fc9cb22318a4096348a23cf3eacbfb6
karthik1987 <karthik.g@naethra.com>
2016-03-01 19:15:55 +0530

    Project uploaded to Git

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=naethra1_attc',
+    'username' => 'naethra1_attc',
+    'password' => 'Login@123',
+    'charset' => 'utf8',
+];
