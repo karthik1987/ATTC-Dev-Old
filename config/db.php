2eb7cc1a24b9b44bd5efa2598b4971db58c1a5fc
karthik1987 <karthik.g@naethra.com>
2016-03-02 15:43:58 +0530

    Project First commit

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=naethra1_attc',
+    'dsn' => 'mysql:host=185.27.133.12;dbname=naethra1_attc',
     'username' => 'naethra1_attc',
     'password' => 'Login@123',
     'charset' => 'utf8',
